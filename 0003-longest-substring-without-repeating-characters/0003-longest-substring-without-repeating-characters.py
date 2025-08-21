62338283e6e4d2a454ae65b90ba5ddf5e8d6715e
Guri-1186 <142791272+Guri-1186@users.noreply.github.com>
2025-08-21 21:16:08 +0400

    Time: 11 ms (96.97%), Space: 12.7 MB (65.91%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,8 +1,7 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        # Dictionary to store the last seen index of each character
         seen = {}
-        left = 0   # left side of sliding window
+        left = 0  
         max_len = 0
 
         for right, char in enumerate(s):
@@ -11,8 +10,6 @@ class Solution(object):
                 left = seen[char] + 1
 
             seen[char] = right
-
-          
             max_len = max(max_len, right - left + 1)
 
         return max_len
